9402d4577025ea25a72ba335411138861f41057e
Sam MacPherson <xylom44@gmail.com>
2020-11-08 14:07:55 -0500

    import VatAbstract

--- src/psm.sol
+++ src/psm.sol
@@ -1,11 +1,17 @@
 pragma solidity ^0.6.7;
 
+import { VatAbstract } from "dss-interfaces/dss/VatAbstract.sol";
+
 contract DssPsm {
 
-    address public vat;
+    VatAbstract public vat;
 
     constructor(address vat_) {
         vat = vat_;
+    }
+
+    function slip(bytes32 ilk, address usr, int256 wad) external note auth {
+        gem[ilk][usr] = add(gem[ilk][usr], wad);
     }
 
 }
