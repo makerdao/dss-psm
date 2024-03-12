a5898b329e7546cf30691f6f7ce62fced35506c6
amusingaxl <112016538+amusingaxl@users.noreply.github.com>
2024-03-12 19:44:00 -0300

    refactor: remove custom test script

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-if [[ -z "$1" ]]; then
-  forge test --rpc-url="$ETH_RPC_URL" --use solc:0.8.14
-else
-  forge test --rpc-url="$ETH_RPC_URL" --use solc:0.8.14 --match "$1" -vvvv
-fi
